cd4c40745ec15b84dace2f9e670512225e11d42f
Mohamedfadah <30502846+Mohamedfadah@users.noreply.github.com>
2022-01-24 01:18:28 +0200

    Make Simple Restful API
    Creating  User, Category, Post, Comment Model and Tables , then migrate them, then enter some data using tinker
    
    https://youtu.be/WDha52dbLWM

---
+++ app/Models/Post.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Post extends Model
+{
+    // use HasFactory;
+    use Sluggable;
+    
+    protected $fillable = [
+        'title', 'content'
+    ];
+
+    
+    /**
+     * return slugable conf array for the model
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+    
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Category;
+use App\Models\Comment;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,73 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+// Users
+Route::get('/users', fn() => User::all());
+Route::post('/users', function ()
+{
+    request()->validate([
+        'name' => 'required',
+        'email' => 'required',
+        'password' => 'required'
+    ]);
+    return User::create([
+        'name' => request('name'),
+        'email' => request('email'),
+        'password' => request('password')
+    ]);
+});
+
+// Posts
+Route::get('/posts', function ()
+{
+    return Post::all();
+});
+Route::post('/posts', function ()
+{
+    request()->validate([
+        'title' => 'required',
+        'content' => 'required'
+    ]);
+    return Post::create([
+        'user_id' => request('user_id'),
+        'cat_id' => request('cat_id'),
+        'title' => request('title'),
+        'content' => request('content')
+    ]);
+});
+Route::put('/posts/{post}', function (Post $post)
+{
+    request()->validate([
+        'title' => 'required',
+        'content' => 'required'
+    ]);
+    $post->update([
+        // 'user_id' => request('user_id'),
+        // 'cat_id' => request('cat_id'),
+        'title' => request('title'),
+        'content' => request('content')
+    ]);
+});
+
+// Categories
+Route::get('/categories', fn() => Category::all());
+Route::post('/categories', function () {
+    request()->validate([ 'name' => 'required' ]);
+    return Category::create([ 'name' => request('name') ]);
+});
+
+// Comments
+Route::get('/comments', fn() => Comment::all());
+Route::post('/comments', function ()
+{
+    request()->validate([
+        'comment' => 'required'
+    ]);
+    return Comment::create([
+        'user_id' => request('user_id'),
+        'post_id' => request('post_id'),
+        'comment' => request('comment'),
+        'vote' => request('vote')
+    ]);
+});
+
